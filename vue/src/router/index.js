7acc378454c288459a013bd2c539b7cceaa56146
宋剑2 <2021302111195@whu.edu.cn>
2024-07-04 09:41:13 +0800

    restore vue folder

---
+++ vue/src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import LayoutView from "@/views/Layout.vue";
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView
+  },
+  {
+    path: '/',
+    name: 'Layout',
+    component: LayoutView,
+    children:[
+      {
+        path: '',
+        name: 'home',
+        component: HomeView
+      },
+      {
+        path: 'camera',
+        name: 'camera',
+        component: () => import(/* webpackChunkName: "about" */ '../views/CameraView.vue')
+      }
+    ]
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ vue/src/views/CameraView.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div>
+      <el-input style="width: 300px; margin-right: 10px" placeholder="请输入内容"></el-input>
+      <el-button type="warning">搜索</el-button>
+      <el-button type="primary">新增</el-button>
+    </div>
+    <div>
+      <el-table :data="tableData" stripe style="width: 100%">
+        <el-table-column prop="date" label="日期" ></el-table-column>
+        <el-table-column prop="name" label="姓名" ></el-table-column>
+        <el-table-column prop="address" label="地址"></el-table-column>
+        <el-table-column label="操作" >
+          <el-button type="primary" icon="el-icon-document">编辑</el-button>
+          <el-button type="danger" icon="el-icon-delete">删除</el-button>
+        </el-table-column>
+      </el-table>
+    </div>
+    <div class="block" style="margin-top: 10px">
+      <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="currentPage4"
+          :page-sizes="[10, 20, 30, 40]"
+          :page-size="10"
+          layout="total, sizes, prev, pager, next"
+          :total="40">
+      </el-pagination>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    }
+  }
+}
+</script>
