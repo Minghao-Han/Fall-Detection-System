8fec40e06de16493b4a1f76307dc65682e424e9f
宋剑2 <2021302111195@whu.edu.cn>
2024-07-12 17:03:41 +0800

    RUD

--- springboot.sql
+++ springboot.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80031 (8.0.31)
  File Encoding         : 65001
 
- Date: 06/07/2024 15:52:05
+ Date: 12/07/2024 17:02:12
 */
 
 SET NAMES utf8mb4;
@@ -22,47 +22,46 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `admin`;
 CREATE TABLE `admin`  (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT '主键ID',
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '姓名',
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '密码',
-  `sex` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '性别',
-  `age` int NULL DEFAULT NULL COMMENT '年龄',
-  `phone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '电话',
-  PRIMARY KEY (`id`) USING BTREE,
+  `phone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '电话',
+  PRIMARY KEY (`phone`) USING BTREE,
   UNIQUE INDEX `phone`(`phone` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '用户信息表' ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '用户信息表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of admin
 -- ----------------------------
-INSERT INTO `admin` VALUES (1, '张三', '123456', '男', 25, '15527685070');
-INSERT INTO `admin` VALUES (2, '李四', '123456', '女', 25, '18815541851');
-INSERT INTO `admin` VALUES (3, '宋剑', '123456', NULL, NULL, NULL);
+INSERT INTO `admin` VALUES ('momo', '123456', '11111111111');
+INSERT INTO `admin` VALUES ('sj', '123456', '12345678900');
+INSERT INTO `admin` VALUES ('宋剑', '123456', '13675542871');
+INSERT INTO `admin` VALUES ('张三', '123456', '15527685070');
+INSERT INTO `admin` VALUES ('李四', '123456', '18815541851');
 
 -- ----------------------------
 -- Table structure for camera
 -- ----------------------------
 DROP TABLE IF EXISTS `camera`;
 CREATE TABLE `camera`  (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT '主键ID',
   `cameraName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '摄像机的名字',
-  `serialNumber` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '摄像机的序列号',
+  `serialNumber` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '摄像机的序列号',
   `phone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '绑定用户电话',
   `dateTime` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '绑定的日期',
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '老人的名字',
-  PRIMARY KEY (`id`) USING BTREE,
+  PRIMARY KEY (`serialNumber`) USING BTREE,
   UNIQUE INDEX `serialnumber`(`serialNumber` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of camera
 -- ----------------------------
-INSERT INTO `camera` VALUES (1, '卧室11', '5JF6Q7GGSE0WDLBK', '15527685070', '2024-07-05 00:00:00', '宋剑');
-INSERT INTO `camera` VALUES (2, '客厅', 'X4dR7pL3H9mN2bQ1', '18815541851', '2024-07-05 00:00:00', '王立琪');
-INSERT INTO `camera` VALUES (3, '卧室2', 'G7kL9mN2pQ8rT4uV', '15527685070', '2024-07-05 00:00:00', '曹倚铭');
-INSERT INTO `camera` VALUES (4, '卧室3', 'G7kL9mN2pZ8rT4uV', '18815541851', '2024-07-05 00:00:00', '韩明昊');
-INSERT INTO `camera` VALUES (5, '卧室4', 'X4dR7pL3H9mN2bQ5', NULL, '2024-07-06 15:42:11', '宋剑');
-INSERT INTO `camera` VALUES (6, '卧室5', '5JF6Q7GGSE0WDLBP', '15527685070', '2024-07-06 10:48:03', '宋剑');
-INSERT INTO `camera` VALUES (7, '卧室6', '5JF6Q7GeSE0WDLBP', '15527685070', '2024-07-06 10:49:03', '宋剑');
+INSERT INTO `camera` VALUES ('卧室11', '5JF6Q7GeSE0WDLBP', '13675542871', '2024-07-11 00:00:00', '宋剑');
+INSERT INTO `camera` VALUES ('卧室11', '5JF6Q7GGSE0WDLBK', '15527685070', '2024-07-05 00:00:00', '宋剑');
+INSERT INTO `camera` VALUES ('卧室5', '5JF6Q7GGSE0WDLBP', '15527685070', '2024-07-06 10:48:03', '宋剑');
+INSERT INTO `camera` VALUES ('寝室', 'A7kL9mN2pQ8rT4uV', '15527685070', '2024-07-06 00:00:00', '张三');
+INSERT INTO `camera` VALUES ('卧室2', 'G7kL9mN2pQ8rT4uV', '15527685070', '2024-07-05 00:00:00', '曹倚铭');
+INSERT INTO `camera` VALUES ('卧室3', 'G7kL9mN2pZ8rT4uV', '18815541851', '2024-07-05 00:00:00', '韩明昊');
+INSERT INTO `camera` VALUES ('客厅2', 'X4dR7pL3H9mN2bQ1', '15527685070', '2024-07-11 17:04:24', '王立');
+INSERT INTO `camera` VALUES ('卧室4', 'X4dR7pL3H9mN2bQ5', '15527685070', '2024-07-06 15:52:09', '宋剑');
 
 SET FOREIGN_KEY_CHECKS = 1;
--- vue/src/utils/request.js
+++ vue/src/utils/request.js
@@ -2,16 +2,20 @@ import axios from 'axios'
 
 // 创建一个axios对象出来
 const request = axios.create({
-    baseURL: 'http://localhost:8080/',
+    baseURL: 'http://localhost:8080/api',
     timeout: 5000
 })
+
 
 // request 拦截器
 // 可以自请求发送前对请求做一些处理
 // 比如统一加token，对请求参数统一加密
 request.interceptors.request.use(config => {
     config.headers['Content-Type'] = 'application/json;charset=utf-8';
-
+    const user = localStorage.getItem("user");
+    if (user) {
+        config.headers['token'] = JSON.parse(user).token;
+    }
     // config.headers['token'] = user.token;  // 设置请求头
     return config
 }, error => {
