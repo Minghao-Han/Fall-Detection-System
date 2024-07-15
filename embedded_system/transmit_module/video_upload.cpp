f514af2b6e9044fe5a2a441509c151481c17e012
DivingClicker <divingclicker@163.com>
2024-07-15 14:25:48 +0800

    Transmit module in embedded system added.

--- embedded_system/transmit_module/video_upload.cpp
+++ embedded_system/transmit_module/video_upload.cpp
@@ -2,17 +2,14 @@
 #include <curl/curl.h>
 #include <fstream>
 
-// 定义上传文件的URL,到时候要改动的。 replace localhost to server's ip
-#define UPLOAD_URL "http://localhost:8080/video/upload"
+#define UPLOAD_URL "http://10.128.149.159:8080/api/video/upload"
 
-// 上传文件的函数
-bool uploadFile(const char* filePath) {
+bool uploadFile(const char* filePath, const char* token) {
     CURL *curl;
     CURLcode res;
     struct curl_httppost *formpost = NULL;
     struct curl_httppost *lastptr = NULL;
     struct curl_slist *headerlist = NULL;
-    static const char buf[] = "Expect:";
 
     curl_global_init(CURL_GLOBAL_ALL);
     curl = curl_easy_init();
@@ -28,6 +25,12 @@ bool uploadFile(const char* filePath) {
         curl_easy_setopt(curl, CURLOPT_URL, UPLOAD_URL);
         curl_easy_setopt(curl, CURLOPT_HTTPPOST, formpost);
 
+        // 添加Token头部
+        std::string authHeader = "Token: ";
+        authHeader += token;
+        headerlist = curl_slist_append(headerlist, authHeader.c_str());
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headerlist);
+
         // 提交请求
         res = curl_easy_perform(curl);
 
@@ -41,6 +44,7 @@ bool uploadFile(const char* filePath) {
         // 清理curl和表单
         curl_easy_cleanup(curl);
         curl_formfree(formpost);
+        curl_slist_free_all(headerlist);
 
         return true;
     }
@@ -48,15 +52,16 @@ bool uploadFile(const char* filePath) {
 }
 
 int main(int argc, char *argv[]) {
-    if(argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <file_path_to_upload>" << std::endl;
+    if(argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " <file_path_to_upload> <token>" << std::endl;
         return 1;
     }
 
     const char* filePath = argv[1];
+    const char* token = argv[2];
 
     // 调用上传文件函数
-    if(uploadFile(filePath)) {
+    if(uploadFile(filePath, token)) {
         std::cout << "File uploaded successfully!" << std::endl;
     } else {
         std::cerr << "Failed to upload file." << std::endl;
@@ -64,3 +69,4 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
+
